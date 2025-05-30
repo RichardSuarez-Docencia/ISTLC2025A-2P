869655ded4900fb5d44698e882f8e8ac25326a8c
Richard <richard.suarezjacome2021@gmail.com>
2025-05-30 07:19:47 -0500

    clase 29/05/2025

---
+++ javascript/Clase05292025/operadoresArimeticos.js
@@ -0,0 +1,29 @@
+let a=75;
+let b=50;
+
+let suma = a + b;
+console.log(`La suma es ${suma}`);
+
+let resta = a - b;
+console.log(`La resta es ${resta}`);
+
+let multiplicacion = a * b;
+console.log(`La multiplicacion es ${multiplicacion}`);
+
+let division = a / b;
+console.log(division);
+
+let procentaje = a % b;
+console.log(procentaje);
+
+let operadorMasIgual =5
+operadorMasIgual += 2;
+console.log(operadorMasIgual);
+
+let operadorMenosIgual =50
+operadorMenosIgual -= 20;
+console.log(operadorMenosIgual);
+
+let operadorMultiplicacionIgual= 25;
+operadorMultiplicacionIgual *= 3;
+console.log(operadorMultiplicacionIgual);
---
+++ javascript/Clase05292025/script.js
@@ -0,0 +1,61 @@
+let nombreA = "Juan"; //String
+let edad= 21;        //number
+console.log(typeof nombre);
+console.log(typeof edad);
+console.log(`Mi nombre es ${nombre} y mi edad es ${edad} años edad`);
+console.log("Mi nombre es "+nombre + "y tengo "+ edad +"años de edad");
+
+let persona = {
+    nombre:"Luis",
+    apellido:"Olvera",
+    edad:14,
+    telefono:"0951489751"
+}
+
+console.log(typeof persona);
+console.log(`Mi nombre es ${persona.nombre}`);
+console.log(`Mi apellido es ${persona.apellido}`);
+console.log(`Mi Edad es ${persona.edad}`);
+console.log(`Mi telefono es ${persona.telefono}`);
+
+let isMayorEdad = true;
+console.log(typeof isMayorEdad);
+
+isMayorEdad = false;
+console.log(typeof isMayorEdad);
+
+function saludar() {
+    return "Hola Mundo JS";
+}
+
+console.log(typeof saludar);
+
+
+class Animal {
+  constructor(nombre) {
+    this.nombre = nombre;
+  }
+}
+
+let suma;
+suma = "dsdsds"
+
+console.log(typeof suma);
+
+let y = null;
+console.log(typeof y);
+
+
+var nombre = "Ana"; 
+var nombre = "Pedro"; //  Permitido (pero confuso) nombre = "Ana";
+console.log(nombre); // "Ana"
+
+let apellidoA = "Ana"; 
+ apellidoA = "Pedro"; //  Permitido (pero confuso) nombre = "Ana";
+console.log(apellidoA);
+
+const PI = 3.14159; // const PI = 3.14; //Error: no se puede redeclarar // PI = 3.14; //Error: no se puede cambiar 
+ //PI = 3.1698;
+console.log(PI); // 3.14159
+
+
